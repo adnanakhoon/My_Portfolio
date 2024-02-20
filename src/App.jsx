98cb1cb91a5e6fe59a7ca4f7f7f6957173a33d9c
adnan <adnanakhoon08@gmail.com>
2024-02-20 20:58:39 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,11 @@ import Themes from "./components/Themes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/My_portfolio">
     <Navbar/>
     <Themes/>
       <Routes>
        <Route index element={<Home/>}/>
-       <Route path="about" element={<About/>}/>
        <Route path="portfolio" element={<Portfolio/>}/>
        <Route path="contact" element={<Contact/>} />
       </Routes>
